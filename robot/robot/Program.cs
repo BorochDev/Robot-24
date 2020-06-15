9a884a3df39db1854c4e5d9a9dd1fa6332054547
Mateusz Boroch <m.boroch2@gmail.com>
2020-06-15 20:20:51 +0200

    Add repo

---
+++ robot/robot/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace robot
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<Robot> Robots = new List<Robot>();
+
+            Robots.Add(new Robot());
+            Robots.Add(new Robot());
+            Robots.Add(new Robot());
+            Robots.Add(new Robot());
+
+            foreach (var item in Robots)
+            {
+                Console.WriteLine(item.Name + " " + item.Type);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ robot/robot/Robot.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace robot
+{
+    enum RobotType
+    {
+        Translator,
+        CoffeeMaking,
+        Bartender
+    }
+    class Robot
+    {
+        public string Name { get; set; }
+        public RobotType Type { get; set; }
+        private static List<string> names = new List<string>();
+        private bool unique { get; set; }
+        private Random random = new Random();
+
+        public Robot()
+        {
+            nameGenerate();
+
+            names.Add(Name);
+            TypeGenerate();
+        }
+
+        public void Reset()
+        {
+            names.Remove(Name);
+
+            nameGenerate();
+
+            names.Add(Name);
+        }
+
+        private void nameGenerate()
+        {
+            do
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    Name += (char)('a' + random.Next(0, 25));
+                }
+                Name += (random.Next(100, 1000)).ToString();
+
+                unique = true;
+
+                foreach (var item in names)
+                {
+                    if (item == Name)
+                    {
+                        unique = false;
+                    }
+                }
+
+            } while (unique == false);
+        }
+
+        private void TypeGenerate()
+        {
+            switch (random.Next(1,4))
+            {
+                case 1:
+                    Type = RobotType.Translator;
+                    break;
+                case 2:
+                    Type = RobotType.CoffeeMaking;
+                    break;
+                case 3:
+                    Type = RobotType.Bartender;
+                    break;
+            }
+        }
+    }
+}
